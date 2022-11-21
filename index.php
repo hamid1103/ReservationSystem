9b2a53c3415165b12e7b4d8a6c02cb480b06dcff
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2022-11-21 14:35:39 +0100

    new stuff

--- index.php
+++ index.php
@@ -1,114 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Science Planner</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="css/bootstrap.css" rel="stylesheet">
 
 </head>
 <body>
-<?php
-// Enable loading of Composer dependencies
-require_once realpath(__DIR__ . '/vendor/autoload.php');
-require_once 'GraphHelper.php';
+<section class="d-flex flex-row mb-3 align-content-center justify-content-center">
+    <!--Section left of form-->
+    <div class="container-lg w-25 border border-dark p-3 mt-3">
+    </div>
 
-// Load .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$dotenv->required(['CLIENT_ID', 'CLIENT_SECRET', 'TENANT_ID', 'AUTH_TENANT', 'GRAPH_USER_SCOPES']);
+    <!--Form in the middle-->
+<div class="container-lg w-25 border border-dark p-3 mt-3">
+    <form>
+        <div class="mb-3">
+            <label for="InputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="m-3">
+            <label for="InputDate" class="form-label">Datum</label>
+            <input type="datetime-local" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="Subject" class="form-label">Subject</label>
+            <input type="text" class="form-control" id="Subject">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 
-initializeGraph();
-
-greetUser();
-
-$choice = -1;
-
-while ($choice != 0) {
-    echo('Please choose one of the following options:');
-    echo('0. Exit');
-    echo('1. Display access token');
-    echo('2. List my inbox');
-    echo('3. Send mail');
-    echo('4. List users (requires app-only)');
-    echo('5. Make a Graph call');
-
-    $choice = (int)readline('');
-
-    switch ($choice) {
-        case 1:
-            displayAccessToken();
-            break;
-        case 2:
-            listInbox();
-            break;
-        case 3:
-            sendMail();
-            break;
-        case 4:
-            listUsers();
-            break;
-        case 5:
-            makeGraphCall();
-            break;
-        case 0:
-        default:
-            print('Goodbye...'.PHP_EOL);
-    }
-}
-
-function initializeGraph(): void {
-    GraphHelper::initializeGraphForUserAuth();
-}
-
-function greetUser(): void {
-    try {
-        $user = GraphHelper::getUser();
-        print('Hello, '.$user->getDisplayName().'!'.PHP_EOL);
-
-        // For Work/school accounts, email is in Mail property
-        // Personal accounts, email is in UserPrincipalName
-        $email = $user->getMail();
-        if (empty($email)) {
-            $email = $user->getUserPrincipalName();
-        }
-        print('Email: '.$email.PHP_EOL.PHP_EOL);
-    } catch (Exception $e) {
-        print('Error getting user: '.$e->getMessage().PHP_EOL.PHP_EOL);
-    }
-}
-
-function displayAccessToken(): void {
-    try {
-        $token = GraphHelper::getUserToken();
-        print('User token: '.$token.PHP_EOL.PHP_EOL);
-    } catch (Exception $e) {
-        print('Error getting access token: '.$e->getMessage().PHP_EOL.PHP_EOL);
-    }
-}
-
-function listInbox(): void {
-    // TODO
-}
-
-function sendMail(): void {
-    // TODO
-}
-
-function listUsers(): void {
-    // TODO
-}
-
-function makeGraphCall(): void {
-    // TODO
-}
-
-?>
+    <!--section right of form-->
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+</section>
+
+
+
+<script src="js/bootstrap.bundle.js"></script>
 </body>
 </html>
