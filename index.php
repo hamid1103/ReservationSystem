b8a9f5c21abc4773f8344a0bbcdaef84bea6ddfd
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2022-12-20 15:18:27 +0100

    updated index navbar

--- index.php
+++ index.php
@@ -77,27 +77,10 @@ try {
                 My Reservations
             </a>
 
-            <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">
-                    More
-                </a>
+            <a class="navbar-item" href="contact.html">
+                Contact
+            </a>
 
-                <div class="navbar-dropdown">
-                    <a class="navbar-item">
-                        About
-                    </a>
-                    <a class="navbar-item">
-                        Jobs
-                    </a>
-                    <a class="navbar-item">
-                        Contact
-                    </a>
-                    <hr class="navbar-divider">
-                    <a class="navbar-item">
-                        Report an issue
-                    </a>
-                </div>
-            </div>
         </div>
 
         <div class="navbar-end">
